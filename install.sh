2489f5ab9635daf5779f2ca7a287aaf943195f7d
zhangml17 <meilongyx@163.com>
2018-08-10 09:31:05 +0200

    第一次修改

--- install.sh
+++ install.sh
@@ -1,15 +1,32 @@
 #!/bin/sh
 
 #download nodejs
-wget https://nodejs.org/dist/v8.11.3/node-v8.11.3.tar.gz 
+#wget https://nodejs.org/dist/v8.11.3/node-v8.11.3.tar.gz
 
-tar -xzvf node-v8.11.3.tar.gz
+#tar -xzvf node-v8.11.3.tar.gz
 
-#install node and express
-cd node-v8.11.3
+#install
+#cd node-v8.11.3
 
-./configure && make && make install && npm install -g express-generator
+#./configure && make && make install
 
-#download node project from github
+#安装express
+#npm install -g express-generator
 
-#run node project
+#从github下载项目
+#wget https://github.com/zhangml17/kuweb/archive/master.zip
+
+#解压
+#判断unzip解压工具是否安装
+#没有，则先安装
+#否则，执行:
+#yum install unzip && unzip ./master.zip
+
+#删除下载的安装包
+#rm -rf node-v8.11.3.tar.gz  master.zip
+
+#判断防火墙是否关闭
+#若没有，则关闭
+#否则，执行：
+cd kuweb-master && npm install && npm start
+
